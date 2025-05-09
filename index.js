9d0303b5d8e52c438a3381f87890a283283a3d9d
Harjeet singh <Harjeetpeelsoo2@gmail.com>
2025-05-09 10:36:55 +0530

    new  feature

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const  btn=document.querySelector("button");
 btn.addEventListener("click",function(){
     console.log("Button  click");
 });
+
+const   btn1=document.querySelector("#btn")
+
+btn1.addEventListener("click",function(){
+    console.log("Sunmit  button Click");
+})
